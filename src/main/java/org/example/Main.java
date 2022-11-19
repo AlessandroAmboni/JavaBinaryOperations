2a9e8a399d87c3f6417b081ad2728862545a19f0
André <andreolive@live.com>
2022-11-19 12:11:34 -0300

    New

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,7 +9,7 @@ import static java.lang.Character.toUpperCase;
 
 public class Main {
     public static void main(String[] args) {
-        binaryAddition("101", "1000");
+        binaryAddition("1010", "1000");
     }
 
     public static void binaryConverter(String binary) {
@@ -35,6 +35,7 @@ public class Main {
     }
 
     public static void binaryAddition(String num1, String num2) {
+        //Convert the binary strings to integers arrayLists
         List<Integer> num1List = num1.chars()
                 .mapToObj(c -> toUpperCase((char) c))
                 .mapToInt(c -> Integer.parseInt(String.valueOf(c)))
@@ -43,16 +44,44 @@ public class Main {
                 .mapToObj(c -> toUpperCase((char) c))
                 .mapToInt(c -> Integer.parseInt(String.valueOf(c)))
                 .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+        //add 0 to the smaller arrayList to make them the same length
         if (num1List.size() > num2List.size()) {
             while (num1List.size() != num2List.size()) {
                 num2List.add(0, 0);
             }
-        } else if (num2List.size() > num1List.size()) {
+        } else {
             while (num2List.size() != num1List.size()) {
                 num1List.add(0, 0);
             }
         }
-        System.out.println(num1List + " " + num2List);
+        System.out.println(num1List + "\n" + num2List);
+        //reverse the ArrayLists for easier addition
+        Collections.reverse(num1List);
+        Collections.reverse(num2List);
+
+        ArrayList<Integer> sumList = new ArrayList<>();
+        int carry = 0;
+        for (int i = 0; i < num1List.size(); i++) {
+            int sum = num1List.get(i) + num2List.get(i) + carry;
+            if (sum == 0) {
+                sumList.add(0);
+                carry = 0;
+            } else if (sum == 1) {
+                sumList.add(1);
+                carry = 0;
+            } else if (sum == 2) {
+                sumList.add(0);
+                carry = 1;
+            } else if (sum == 3) {
+                sumList.add(1);
+                carry = 1;
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        for(Integer i : sumList){
+            result.append(i);
+        }
+
 
     }
 }
